0f31b1f7e4d7d4d7e7679eccecaab41dd1fc4e8d
Simoné Van Zyl <18395317@sun.ac.za>
2018-08-28 12:57:20 +0200

    Flask implementation to dynamically draw graph. Still work in progress

---
+++ Flask/flask_example.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import numpy as np
+import pandas
+import matplotlib.pyplot as plt
+#import dynamic_graph as dg 
+
+app = Flask(__name__)
+
+@app.route('/test')
+def chartTest():
+    d = dg.DynamicUpdate()
+    d()
+    lnprice=np.log(price)
+    plt.plot(lnprice)   
+    plt.savefig('/static/images/new_plot.png')
+    return render_template('untitled1.html', name = 'new_plot',
+            url='/static/images/new_plot.png')
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
+
