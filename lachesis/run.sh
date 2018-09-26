2e167a1e961cb133c1e72e8a203d913d080d57fb
root <root@ip-172-31-9-175.us-east-2.compute.internal>
2018-09-26 10:20:48 +0000

    run.sh changed

--- lachesis/run.sh
+++ lachesis/run.sh
@@ -13,22 +13,20 @@
 docker network create -d bridge --subnet=172.30.100.0/24 hashgraph_net
 docker network ls
 
-docker run --rm --name node1 --ip 172.30.100.101 --network="hashgraph_net" node1 &
-docker run --rm --name node2 --ip 172.30.100.102 --network="hashgraph_net" node2 &
-docker run --rm --name node3 --ip 172.30.100.103 --network="hashgraph_net" node3 &
+docker run --rm --name node1-run --ip 172.30.100.101 --network="hashgraph_net" node1 &
+docker run --rm --name node2-run --ip 172.30.100.102 --network="hashgraph_net" node2 &
+docker run --rm --name node3-run --ip 172.30.100.103 --network="hashgraph_net" node3 &
 sleep 5 
 docker network inspect hashgraph_net 
 echo PING: 
-docker exec node2 ping node1 &
-
-docker network connect hashgraph_net node1 &
-docker network connect hashgraph_net node2 &
-docker network connect hashgraph_net node3 &
-
-docker exec node2 ping node1 &
+docker exec node2-run ping node1-run &
 read input
+#docker network connect hashgraph_net node1 &
+#docker network connect hashgraph_net node2 &
+#docker network connect hashgraph_net node3 &
 
-docker kill node1
-docker kill node2
-docker kill node3
+
+docker kill node1-run
+docker kill node2-run
+docker kill node3-run
 docker network rm hashgraph_net
