4855aed26d6b1f8b9b0b73b7ba6c57d60cc70171
Oliver Gilbey <17839688@sun.ac.za>
2018-10-20 15:16:02 +0200

    Javascript Sliders with values

---
+++ HashgraphVisualizerSDK/testflask.py
@@ -0,0 +1,12 @@
+import socket
+from flask import Flask, render_template
+app = Flask(__name__)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+@app.route("/")
+def hello():
+    return render_template('dapp.html', SliderVal1 = 0, SliderVal2 = 0, SliderVal3 = 0, SliderVal4 = 0, SliderVal5 = 0, SliderVal6 = 0, SliderVal7 = 0, SliderVal8 = 0, SliderVal9 = 0, SliderVal10 = 0, TokenVal = 100, name =
+            'DApp')
+
+app.run(host='0.0.0.0', port=5001)
