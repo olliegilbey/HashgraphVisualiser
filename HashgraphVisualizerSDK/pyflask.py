95575cde407f0d94dd61ea1acec4f4ca262965a2
Oliver Gilbey <17839688@sun.ac.za>
2018-10-20 12:18:31 +0200

    Dappflask and pyflask to dapp branch

--- HashgraphVisualizerSDK/pyflask.py
+++ HashgraphVisualizerSDK/pyflask.py
@@ -10,9 +10,4 @@ def hello():
     return render_template('untitled.html', SliderVal = 4, name =
             'Live Visualizer', graphJSON = "")
 
-@app.route("/dapp")
-def dapp():
-	return render_template('dapp.html', TokenVal = 100, name ='Vote DApp')
-
-
 app.run(host='0.0.0.0', port=5001)
