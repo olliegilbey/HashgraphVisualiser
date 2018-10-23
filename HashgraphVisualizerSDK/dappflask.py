13db93cfa9f633ccc6ca6995feafec4d5a3f6698
Ubuntu <ubuntu@ip-172-31-9-175.us-east-2.compute.internal>
2018-10-23 10:11:56 +0000

    syncing with AWS and master with dapp branch merged

--- HashgraphVisualizerSDK/dappflask.py
+++ HashgraphVisualizerSDK/dappflask.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
-@app.route("/dapp")
+@app.route("/")
 def dapp():
-	return render_template('dapp.html', TokenVal = 100, name ='Vote DApp')
+        return render_template('dapp.html', TokenVal = 100, SliderVal1 = 0, SliderVal2 = 0, SliderVal3 = 0, SliderVal4 = 0, SliderVal5 = 0, SliderVal6 = 0, SliderVal7 = 0, SliderVal8 = 0, SliderVal9 = 0, SliderVal10 = 0, name = "DApp")
 
-@app.route('/')
+@app.route('/hello')
 def hello_world():
 	return 'Hello, World!'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
