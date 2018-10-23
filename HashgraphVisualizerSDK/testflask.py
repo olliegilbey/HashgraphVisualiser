b8d7c80093c043b2eb0a2d7c30b8c2f6c94dc397
Ubuntu <ubuntu@ip-172-31-9-175.us-east-2.compute.internal>
2018-10-23 10:18:34 +0000

    testing flask server filed updated for working css

--- HashgraphVisualizerSDK/testflask.py
+++ HashgraphVisualizerSDK/testflask.py
@@ -1,5 +1,5 @@
 import socket
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,4 +9,4 @@ def hello():
     return render_template('dapp.html', SliderVal1 = 0, SliderVal2 = 0, SliderVal3 = 0, SliderVal4 = 0, SliderVal5 = 0, SliderVal6 = 0, SliderVal7 = 0, SliderVal8 = 0, SliderVal9 = 0, SliderVal10 = 0, TokenVal = 100, name =
             'DApp')
 
-app.run(host='0.0.0.0', port=5001)
+app.run(host='0.0.0.0', port=5000)
