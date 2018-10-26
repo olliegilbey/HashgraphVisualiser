da7a91647a073af26a24bd3c35fd62da9580b48e
Oliver Gilbey <17839688@sun.ac.za>
2018-10-26 12:49:00 +0200

    Host error fixed

--- HashgraphVisualizerSDK/dappflask.py
+++ HashgraphVisualizerSDK/dappflask.py
@@ -85,5 +85,5 @@ w3 = Web3(HTTPProvider("http://127.0.0.1:8545"))
 contract_instance = deploy(len(studentNums))
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
-app.run(debug=True)
+#app.run(debug=True)
 app.run(host='0.0.0.0', port=5000)
